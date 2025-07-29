2e967e69a8babf1c8309ad19dc62334f951967aa
RONITHSINGHVI12 <95407433+RONITHSINGHVI12@users.noreply.github.com>
2025-07-29 16:45:56 +0530

    Add files via upload

---
+++ python81.py
@@ -0,0 +1,19 @@
+set={1,2,3,4,5,1,2,3,4,5}
+set1={1,1,5,6,2,3,5,2,1,6}
+set2={1,"PROSINGHVI", 3,4,5.8,7.9, False}
+print(set2)
+print(set1)
+print(set)
+set1.pop()
+print(set1)
+set1={"red", "Blue", "orange"}
+set2={"Blue", "green"}
+setz = set1.intersection(set2)
+print(setz)
+setx=set1.union(set2)
+print(setx)
+my_array = [1, 3, 5, 3, 7, 9, 3]
+occurrences_of_three = my_array.count(3)
+print(f"Number of occurrences of 3: {occurrences_of_three}")
+reversed_array = my_array[::-1]
+print(f"Reversed array: {reversed_array}")
